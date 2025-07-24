bf2d88fb6497e0ef3fb75b4a146e371b80373aa6
= <jhowell45@googlemail.com>
2025-07-24 13:18:00 +0100

    moved env file and updated docker compose and installed packages to make ife easier.

--- example-api/app/dependencies/config.py
+++ example-api/app/dependencies/config.py
@@ -1,22 +1,22 @@
 from typing import Annotated
 
 from fastapi import Depends
-from pydantic import PostgresDsn, computed_field
+from pydantic import Field, PostgresDsn, computed_field
 from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     # General:
-    ENCRYPTION_KEY: str
+    ENCRYPTION_KEY: str = Field(alias="API_ENCRYPTION_KEY")
 
     # Database:
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_NAME: str
-    DB_ENABLE_SSL: bool = True
+    DB_HOST: str = Field(alias="API_DB_HOST")
+    DB_PORT: int = Field(alias="API_DB_PORT")
+    DB_USER: str = Field(alias="API_DB_USER")
+    DB_PASSWORD: str = Field(alias="API_DB_PASSWORD")
+    DB_NAME: str = Field(alias="API_DB_NAME")
+    DB_ENABLE_SSL: bool = Field(alias="API_DB_ENABLE_SSL", default=False)
 
     @computed_field  # type: ignore[prop-decorator]
     @property
