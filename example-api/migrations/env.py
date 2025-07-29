1d67ca92b0806c9af93fd2a4a115815d01986db0
= <jhowell45@googlemail.com>
2025-07-29 13:17:07 +0100

    updating how tables are monitored and included th embeddings file.

--- example-api/migrations/env.py
+++ example-api/migrations/env.py
@@ -5,8 +5,7 @@ from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
 from app.dependencies.config import get_settings
-from app.models.films import Film, FilmGenre  # noqa
-from app.models.users import User  # noqa
+from app.models import films, users, vector_embeddings  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
