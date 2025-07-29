c7ae9910a5ac055c0807aa219489f0a8bd1f264c
= <jhowell45@googlemail.com>
2025-07-29 13:16:36 +0100

    creating embedding table model.

--- example-api/app/models/vector_embeddings.py
+++ example-api/app/models/vector_embeddings.py
@@ -1,8 +1,12 @@
+from pgvector.sqlalchemy import Vector
 from sqlmodel import Field, SQLModel
+
+from .traits import DateTimestamps
 
 
 class EmbeddingBase(SQLModel): ...
 
 
-class Embedding(EmbeddingBase, table=True):
+class Embedding(EmbeddingBase, DateTimestamps, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    embedding: list[float] = Field(sa_type=Vector())
