0732e05559e36cf20cc8317526c3aa9fd1251f99
= <jhowell45@googlemail.com>
2025-07-30 13:13:14 +0100

    added migration for adding relationship for vectors.

--- example-api/app/models/films.py
+++ example-api/app/models/films.py
@@ -4,6 +4,7 @@ from pydantic import computed_field
 from sqlmodel import Field, Relationship, SQLModel
 
 from .traits import DateTimestamps
+from .vector_embeddings import Embedding, FilmEmbeddingPublic
 
 
 class PublicBase(SQLModel):
@@ -55,14 +56,18 @@ class Film(FilmBase, DateTimestamps, table=True):
     director_id: int = Field(foreign_key="filmdirector.id")
     director: FilmDirector = Relationship(back_populates="films")
 
+    embedding_id: int = Field(foreign_key="embedding.id")
+    embedding: Embedding = Relationship(back_populates="film")
+
     @computed_field(repr=True)
-    def vector_text(self) -> str:
+    def embedding_text(self) -> str:
         return f"The name of this film is '{self.name}' and it was released in {self.release_year} It is about {self.overview}. The film is {self.runtime_minutes} minutes long and has an IMDB rating of {self.imdb_rating} and a meta score of {self.meta_score}."
 
 
 class FilmPublic(FilmBase, PublicBase):
     director: FilmDirectorPublic
-    vector_text: str
+    embedding_text: str
+    embedding: FilmEmbeddingPublic
 
 
 class FilmCreate(FilmBase):
--- example-api/app/models/vector_embeddings.py
+++ example-api/app/models/vector_embeddings.py
@@ -1,7 +1,12 @@
+from typing import TYPE_CHECKING
+
 from pgvector.sqlalchemy import Vector
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 from .traits import DateTimestamps
+
+if TYPE_CHECKING:
+    from app.models.films import Film
 
 
 class EmbeddingBase(SQLModel): ...
@@ -10,3 +15,10 @@ class EmbeddingBase(SQLModel): ...
 class Embedding(EmbeddingBase, DateTimestamps, table=True):
     id: int | None = Field(default=None, primary_key=True)
     embedding: list[float] = Field(sa_type=Vector())
+
+    film: "Film" = Relationship(back_populates="embedding")
+
+
+class FilmEmbeddingPublic(EmbeddingBase):
+    id: int
+    embedding: list[float]
