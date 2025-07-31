bd17e9cba6c3a250a3b5851dd8f423a0df43a0d2
= <jhowell45@googlemail.com>
2025-08-01 00:42:38 +0100

    add embedding text to the response model.

--- example-api/app/routes/api/v1/responses/embeddings.py
+++ example-api/app/routes/api/v1/responses/embeddings.py
@@ -4,6 +4,7 @@ from app.models.vector_embeddings import EmbeddingBase
 
 class EmbeddingFilmPublic(FilmBase, PublicBase):
     director: FilmDirectorPublic
+    embedding_text: str
 
 
 class EmbeddingPublic(EmbeddingBase):
