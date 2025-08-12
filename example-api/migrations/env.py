017c3ad01a2989b26bb175b748e85aaa3c6f42a5
= <jhowell45@googlemail.com>
2025-08-12 23:20:21 +0100

    start tracking migration watch for the embedding models.

--- example-api/migrations/env.py
+++ example-api/migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
 from app.dependencies.config import get_settings
-from app.models import films, users  # noqa
+from app.models import embeddings, films, users  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
